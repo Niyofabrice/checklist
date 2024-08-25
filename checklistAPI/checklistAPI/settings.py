593c124ff10d6d81f87462c4ad9b044fa006ff20
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-08-25 21:57:05 +0200

    setting up postgres database

--- checklistAPI/checklistAPI/settings.py
+++ checklistAPI/checklistAPI/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'checklistAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'checklist',
+        'USER': 'postgres',
+        'PASSWORD': '10990',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
