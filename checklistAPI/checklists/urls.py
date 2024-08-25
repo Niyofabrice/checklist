a4aa32a53336f3986bdbc9605d130e43b370aae8
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-08-25 22:18:21 +0200

    setting up some of the authentication routes

--- checklistAPI/checklists/urls.py
+++ checklistAPI/checklists/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.contrib.auth import views as auth_views
+
 from .views import RegistrationView
 from .views import ChecklistItemViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'checklist-items', ChecklistItemViewSet)
@@ -9,4 +12,15 @@ router.register(r'checklist-items', ChecklistItemViewSet)
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('api/', include(router.urls)),
+
+
+    path('accounts/signup/', views.signup, name='signup'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
